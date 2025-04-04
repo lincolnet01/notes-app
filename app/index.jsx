0199f020af1dfbe5fae0dff1b397df466c8d0fc3
lincolnet01 <kamanoabra2014@outlook.com>
2025-04-04 01:16:24 +0000

    Customized the Home screen

---
+++ app/index.jsx
@@ -0,0 +1,42 @@
+import { Text, View, StyleSheet, Image } from "react-native";
+import PostItImage from "@/assets/images/post-it.png";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={PostItImage} style={styles.image} />
+      <Text style={styles.title}>Welcome to Notes App</Text>
+      <Text style={styles.subtitle}>
+        Capture your thoughts anytime, anywhere!
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#333",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+});
+
+export default HomeScreen;
